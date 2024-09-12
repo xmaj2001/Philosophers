230de3866c6cf1533a0d7f253fcf3a27d17c1a3c
xmaj2001 <xmaj2001@gmail.com>
2024-09-12 11:42:43 -0400

    update task

--- tasks.c
+++ tasks.c
@@ -6,7 +6,7 @@
 /*   By: xjose <xjose@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 13:33:47 by xjose             #+#    #+#             */
-/*   Updated: 2024/09/12 11:39:20 by xjose            ###   ########.fr       */
+/*   Updated: 2024/09/12 11:42:08 by xjose            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,19 +35,20 @@ void	*task_death(void *data)
 	philo = (t_philo *)data;
 	while (!philo->sys->system)
 	{
-		pthread_mutex_lock(&philo->cheack);
-		//pthread_mutex_lock(&philo->sys->system_mutex);
+		pthread_mutex_lock(&philo->sys->system_mutex);
 		if (get_time_now() - philo->last_time_eat >= philo->sys->time_to_death
 			&& philo->sys->system == 0)
 		{
 			printf("\033[0;31mTIME[%lld] PHILO (%d) %s\n", time_ms(philo),
 				philo->id + 1, "CUBOU\033[0m");
+			if (philo->left_fork)
+				pthread_mutex_unlock(philo->left_fork);
+			if (philo->right_fork)
+				pthread_mutex_unlock(philo->right_fork);
 			philo->sys->system = 1;
 		}
-		//pthread_mutex_unlock(&philo->sys->system_mutex);
-		pthread_mutex_unlock(&philo->cheack);
+		pthread_mutex_unlock(&philo->sys->system_mutex);
 		usleep(100);
 	}
-			
 	return (NULL);
 }
