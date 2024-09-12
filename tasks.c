06299d66f66df47d66ec5fb1d0210745dd003a16
xmaj2001 <xmaj2001@gmail.com>
2024-09-12 10:19:02 -0400

    update project

--- tasks.c
+++ tasks.c
@@ -6,7 +6,7 @@
 /*   By: xjose <xjose@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 13:33:47 by xjose             #+#    #+#             */
-/*   Updated: 2024/09/12 10:02:44 by xjose            ###   ########.fr       */
+/*   Updated: 2024/09/12 10:18:45 by xjose            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	*task_eat(void *data)
 	{
 		pthread_mutex_lock(&sys->system_mutex);
 		if (sys->count_philo_eat == sys->nbr_philos)
-            sys->system = 1;
+			sys->system = 1;
 		pthread_mutex_unlock(&sys->system_mutex);
 		usleep(100);
 	}
@@ -30,23 +30,23 @@ void	*task_eat(void *data)
 
 void	*task_death(void *data)
 {
-	t_philo *philo;
-    long long time;
+	t_philo		*philo;
+	long long	time;
 
 	philo = (t_philo *)data;
 	while (!philo->sys->system)
 	{
 		pthread_mutex_lock(&philo->cheack);
-        time = get_time_now() - philo->last_time_eat;
+		time = get_time_now() - philo->last_time_eat;
 		pthread_mutex_lock(&philo->sys->system_mutex);
 		if (time == philo->sys->time_to_death && philo->sys->system == 0)
 		{
-			printf("\033[0;31mTIME[%lld] PHILO (%d) %s\n\033[0m ", time_ms(philo), philo->id
-			+ 1, "\033[0;31mCUBOU\033[0m");
+			printf("\033[0;31mTIME[%lld] PHILO (%d) %s\n", time_ms(philo),
+				philo->id + 1, "CUBOU\033[0m");
 			philo->sys->system = 1;
 		}
 		pthread_mutex_unlock(&philo->sys->system_mutex);
-        pthread_mutex_unlock(&philo->cheack);
+		pthread_mutex_unlock(&philo->cheack);
 		usleep(100);
 	}
 	if (philo->left_fork)
