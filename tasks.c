7678bc8e021b63845e5620c760831c9467ef7453
xmaj2001 <xmaj2001@gmail.com>
2024-09-13 09:17:53 -0400

    update task

--- tasks.c
+++ tasks.c
@@ -6,7 +6,7 @@
 /*   By: xjose <xjose@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 13:33:47 by xjose             #+#    #+#             */
-/*   Updated: 2024/09/13 09:15:16 by xjose            ###   ########.fr       */
+/*   Updated: 2024/09/13 09:17:32 by xjose            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,14 +39,12 @@ void	*task_death(void *data)
 		if (get_time_now() - philo->last_time_eat >= philo->sys->time_to_death
 			&& philo->sys->system == 0)
 		{
-			pthread_mutex_lock(&philo->cheack);
 			printf("\033[0;31mTIME[%lld] PHILO (%d) %s\n", time_ms(philo),
 				philo->id + 1, "CUBOU\033[0m");
-			pthread_mutex_unlock(&philo->cheack);
-			if (philo->right_fork)
-				pthread_mutex_unlock(philo->right_fork);
-			if (philo->left_fork)
-				pthread_mutex_unlock(philo->left_fork);
+			// if (philo->right_fork)
+			// 	pthread_mutex_unlock(philo->right_fork);
+			// if (philo->left_fork)
+			// 	pthread_mutex_unlock(philo->left_fork);
 			philo->sys->system = 1;
 		}
 		pthread_mutex_unlock(&philo->sys->system_mutex);
