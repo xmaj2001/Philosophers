0c9ebd37340b3a406743fbc6608b05fe80da8a4b
xmaj2001 <xmaj2001@gmail.com>
2024-09-13 07:23:51 -0400

    update task

--- philo_utils.c
+++ philo_utils.c
@@ -6,7 +6,7 @@
 /*   By: xjose <xjose@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/03 12:57:17 by xjose             #+#    #+#             */
-/*   Updated: 2024/09/13 07:21:13 by xjose            ###   ########.fr       */
+/*   Updated: 2024/09/13 07:23:06 by xjose            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,7 @@ long long	time_ms(t_philo *philo)
 }
 void	print_states(t_philo *philo, char *states)
 {
-	pthread_mutex_lock(&philo->sys->system_mutex);
 	if (!philo->sys->system)
 		printf("TIME[\033[0;33m%lld\033[0m] PHILO {%d} %s\t\n", time_ms(philo), philo->id
 			+ 1, states);
-	pthread_mutex_unlock(&philo->sys->system_mutex);
 }
